abbde43e6bef2dcda0b9bdf8633e6d33b9d6c7cc
Artur Mikhaylov <v.artur.mikhaylov@reddit.com>
2023-07-15 19:36:58 +0300

    Initial Commit

---
+++ PodsCacheExample/PodsCacheExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodsCacheExampleApp.swift
+//  PodsCacheExample
+//
+//  Created by Arthur Mikhaylov on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PodsCacheExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
